ed962fb63d21169a14ee992930ee2884731fece1
novimatrem <TheNovimatrem@protonmail.ch>
2020-07-17 21:09:19 +0100

    Initial upload

---
+++ slk.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+amixer set Master mute
+amixer -q -D pulse sset Master mute
+pactl set-sink-mute 0 1
+wmctrl -k on
+sleep 0.2s
+loginctl lock-session
+xdg-screensaver lock
+sleep 0.3s
+dm-tool lock
+exit
