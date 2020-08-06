210af08bb03ea223abaf52e2d658f702210cfc26
Novimatrem <thenovimatrem@protonmail.ch>
2020-08-06 15:52:20 +0000

    Improve

--- slk.sh
+++ slk.sh
@@ -9,4 +9,48 @@ loginctl lock-session
 xdg-screensaver lock
 sleep 0.3s
 dm-tool lock
+
+amixer set Master mute
+amixer -q -D pulse sset Master mute
+pactl set-sink-mute 0 1
+pactl set-sink-mute 1 1
+wmctrl -k on
+sleep 0.2s
+loginctl lock-session
+xdg-screensaver lock
+sleep 0.3s
+dm-tool lock
+
+amixer set Master mute
+amixer -q -D pulse sset Master mute
+pactl set-sink-mute 0 1
+pactl set-sink-mute 1 1
+wmctrl -k on
+sleep 0.2s
+loginctl lock-session
+xdg-screensaver lock
+sleep 0.3s
+dm-tool lock
+
+amixer set Master mute
+amixer -q -D pulse sset Master mute
+pactl set-sink-mute 0 1
+pactl set-sink-mute 1 1
+wmctrl -k on
+sleep 0.2s
+loginctl lock-session
+xdg-screensaver lock
+sleep 0.3s
+dm-tool lock
+
+amixer set Master mute
+amixer -q -D pulse sset Master mute
+pactl set-sink-mute 0 1
+pactl set-sink-mute 1 1
+wmctrl -k on
+sleep 0.2s
+loginctl lock-session
+xdg-screensaver lock
+sleep 0.3s
+dm-tool lock
 exit
